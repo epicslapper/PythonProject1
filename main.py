243b77ad1b8fb20f386eb575497b2973675a2385
peter <pwmbax@gmail.com>
2025-12-18 14:20:09 +0100

    third commit

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyC')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import streamlit as st
+st.write("Hello, World! 🎈")   
